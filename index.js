45d96ce7b841319f352796b609716d4ba2bc27e1
Benjamin Kandaris <benjaminkandaris@yahoo.com>
2021-11-22 15:05:29 -0500

    mongo connected, server running

--- index.js
+++ index.js
@@ -2,3 +2,20 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const cors = require('cors');
+app.use(cors());
+app.use(express.json());
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log('mongo connected');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log('backend running');
+});
