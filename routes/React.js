7ba680316ac9dd6a12939733afbcc742dbf051c6
Benjamin Kandaris <benjaminkandaris@yahoo.com>
2021-11-22 18:56:55 -0500

    finished react/redux routes

---
+++ routes/React.js
@@ -0,0 +1,63 @@
+const React = require('../models/ReactQuestions');
+const router = require('express').Router();
+
+// Create Questions
+router.post('/', async (req, res) => {
+  const newQuestion = new React(req.body);
+
+  try {
+    const savedQuestion = await newQuestion.save();
+    res.status(200).json(savedQuestion);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Read Questions
+// by id
+router.get('/find/:id', async (req, res) => {
+  try {
+    const question = await React.findById(req.params.id);
+    res.status(200).json(question);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// all questions
+router.get('/', async (req, res) => {
+  try {
+    const questions = await React.find();
+    res.status(200).json(questions);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Update Questions
+router.put('/:id', async (req, res) => {
+  try {
+    const updatedQuestion = await React.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedQuestion);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Delete Questions
+router.delete('/:id', async (req, res) => {
+  try {
+    await React.findByIdAndDelete(req.params.id);
+    res.status(200).json('Question has been deleted');
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
