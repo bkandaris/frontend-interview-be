8a846544ffe2864ace8c7a060b5c266215d3d716
Benjamin Kandaris <benjaminkandaris@yahoo.com>
2021-11-22 16:10:56 -0500

    all crud done for javascript questions

--- routes/JavaScript.js
+++ routes/JavaScript.js
@@ -13,4 +13,51 @@ router.post('/', async (req, res) => {
   }
 });
 
+// Read Questions
+// by id
+router.get('/find/:id', async (req, res) => {
+  try {
+    const question = await JavaScript.findById(req.params.id);
+    res.status(200).json(question);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// all questions
+router.get('/', async (req, res) => {
+  try {
+    const questions = await JavaScript.find();
+    res.status(200).json(questions);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Update Questions
+router.put('/:id', async (req, res) => {
+  try {
+    const updatedQuestion = await JavaScript.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedQuestion);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Delete Questions
+router.delete('/:id', async (req, res) => {
+  try {
+    await JavaScript.findByIdAndDelete(req.params.id);
+    res.status(200).json('Question has been deleted');
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
