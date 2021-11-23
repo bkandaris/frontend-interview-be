55a129449f0bb80475d1e6dac6dd5b95fb91535b
Benjamin Kandaris <benjaminkandaris@yahoo.com>
2021-11-23 13:48:29 -0500

    hopefully fixed heroku

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const cors = require('cors');
 app.use(cors());
 app.use(express.json());
 dotenv.config();
+const PORT = process.env.PORT;
 
 mongoose
   .connect(process.env.MONGO_URL)
@@ -24,7 +25,6 @@ mongoose
 app.use('/api/javascript', javascriptRoute);
 app.use('/api/user', userRoute);
 
-app.listen(process.env.PORT || 5000, () => {
-  console.log('backend running');
-  console.log(process.env.PORT);
+app.listen(PORT || 5000, () => {
+  console.log(`backend running on ${PORT}`);
 });
