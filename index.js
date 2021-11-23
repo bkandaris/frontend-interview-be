3027ef1538432255be6c5adf683ffb25eb1b2fd2
Benjamin Kandaris <benjaminkandaris@yahoo.com>
2021-11-23 14:09:44 -0500

    please work

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const cors = require('cors');
 app.use(cors());
 app.use(express.json());
 dotenv.config();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 mongoose
   .connect(process.env.MONGO_URL)
@@ -25,6 +25,6 @@ mongoose
 app.use('/api/javascript', javascriptRoute);
 app.use('/api/user', userRoute);
 
-app.listen(PORT || 5000, () => {
+app.listen(PORT, () => {
   console.log(`backend running on ${PORT}`);
 });
